920df550d6dcdcfa6fab60db632df97bfa8f0850
Gimlihobanka <gimlihobanka@gmail.com>
2020-10-25 20:25:33 +0100

    Delay na 2000

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,9 @@ void loop() {
 
   digitalWrite(0, HIGH); // sets the digital pin 13 on
   Serial.println("LED On");
-  delay(1000);            // waits for a second
+  delay(2000);            // waits for a second
   digitalWrite(0, LOW);  // sets the digital pin 13 off
   Serial.println("LED Off");
-  delay(1000);            // waits for a second
+  delay(2000);            // waits for a second
   // put your main code here, to run repeatedly:
 }
